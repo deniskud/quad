828a583ca5d28ff91f3637d3bf22e6fae4acffe0
deniskud <denis.kudriakov@gmail.com>
2019-04-07 19:12:17 +0000

    add managment i2c lna & commutator

--- quad.c
+++ quad.c
@@ -34,15 +34,29 @@ double cpu_time_used;
     return 1;
   }
   int nb_samples=0;
-  double gain = 13;    // 0..73
+  double gain = 24;    // 0..73
   unsigned int freq = 433000000; // ? 100fm
   double bandwidth_calibrating = 5000000;
   double sample_rate = 5000000;
   double host_sample_rate;         //
-  int maxnumber=10000000;               // 1 sec
+  int maxnumber=300;               // 1 sec
 
 ////////////////////////////////        INIT I2C         ////////////////
-
+  int devlna[5]; 
+  int extlnaon(int ch){if (ch>0) wiringPiI2CWriteReg8 (devlna[ch], 9, 0x7f);}  // set @channal lna=ON  att=0 filters=bypass 
+  int extlnaoff(int ch){if (ch>0) wiringPiI2CWriteReg8 (devlna[ch], 9, 0xff);} // set  @channal lna=OFF att=0 filters=bypass
+  int extlnaallon() {for (int i=1;i<5;i++) wiringPiI2CWriteReg8 (devlna[i], 9, 0x7f);}// set all lna=ON  att=0 filters=bypass 
+  int extlnaalloff(){for (int i=1;i<5;i++) wiringPiI2CWriteReg8 (devlna[i], 9, 0xff);}// set all lna=OFF  att=0 filters=bypass     
+  int setant1(){wiringPiI2CWriteReg8 (devlna[0], 9, 0x18);} // Set antenna pair 1-2 1-4 (xS  yS)
+  int setant2(){wiringPiI2CWriteReg8 (devlna[0], 9, 0x0c);} // Set antenna pair 1-3 1-5 (xL  yL)
+  int setant3(){wiringPiI2CWriteReg8 (devlna[0], 9, 0x71);} // Set antenna pair 8-7 8-6 (x2S y2S)
+  int setant4(){wiringPiI2CWriteReg8 (devlna[0], 9, 0x53);} // Set antenna pair 8-5 8-3 (x2L e2L)
+//////////////////// init
+  wiringPiSetup();
+  for (int i=0;i<5;i++) {
+    devlna[i] = wiringPiI2CSetup(0x20+i);
+    wiringPiI2CWriteReg8 (devlna[i], 0, 0);
+  }
 ////////////////////////////////        INIT LIME       /////////////////
   int errcnt=0;
   lms_device_t* devicex = NULL;
@@ -60,10 +74,10 @@ double cpu_time_used;
   errcnt+=  LMS_EnableChannel(devicex, LMS_CH_RX, 1, true); //enable RX-1 channel
   errcnt+=  LMS_EnableChannel(devicey, LMS_CH_RX, 0, true); //enable RX-0 channel
   errcnt+=  LMS_EnableChannel(devicey, LMS_CH_RX, 1, true); //enable RX-1 channel
-  errcnt+=  LMS_EnableChannel(devicex, LMS_CH_TX, 0, false); //enable RX-0 channel
-  errcnt+=  LMS_EnableChannel(devicex, LMS_CH_TX, 1, false); //enable RX-1 channel
-  errcnt+=  LMS_EnableChannel(devicey, LMS_CH_TX, 0, false); //enable RX-0 channel
-  errcnt+=  LMS_EnableChannel(devicey, LMS_CH_TX, 1, false); //enable RX-1 channel
+//  errcnt+=  LMS_EnableChannel(devicex, LMS_CH_TX, 0, false); //enable RX-0 channel
+//  errcnt+=  LMS_EnableChannel(devicex, LMS_CH_TX, 1, false); //enable RX-1 channel
+//  errcnt+=  LMS_EnableChannel(devicey, LMS_CH_TX, 0, false); //enable RX-0 channel
+//  errcnt+=  LMS_EnableChannel(devicey, LMS_CH_TX, 1, false); //enable RX-1 channel
   errcnt+=  LMS_SetSampleRate(devicex, sample_rate, 0); //set sample rate for oall channals fo X-device
   errcnt+=  LMS_SetSampleRate(devicey, sample_rate, 0); //set sample rate for oall channals fo Y-device
   errcnt+=  LMS_SetAntenna( devicex, LMS_CH_RX, 0, 2 );//1=RX LNA_H 2=RX LNA_L 3=RX LNA_W 
@@ -136,25 +150,21 @@ double cpu_time_used;
   double maxinbuf3=0,midinbuf3=0;
   double maxinbuf4=0, midinbuf4=0;
   double amp1=0, amp2=0, amp3=0, amp4=0;
-  lms_stream_meta_t metadata1;
-  lms_stream_meta_t metadata2;
-  lms_stream_meta_t metadata3;
-  lms_stream_meta_t metadata4;
+  lms_stream_meta_t metadata1, metadata2, metadata3, metadata4;
   long int prevtsx=0;
   long int prevtsy=0;
   printf("maxnumber=%d\n",maxnumber);
   sstart = clock(); //debug time
   int mn = maxnumber;
   char chin;
+  extlnaalloff();// all LNA=OFF
   while( maxnumber!=0 ) {   ///////////////////    start main loop
     maxnumber--;
     start = clock(); //debug time
-
     nb_samples = LMS_RecvStream( &rx_streamxa, buffxa, buffer_size, &metadata1, 1000 );
     nb_samples = LMS_RecvStream( &rx_streamxb, buffxb, buffer_size, &metadata2, 1000 );
     nb_samples = LMS_RecvStream( &rx_streamya, buffya, buffer_size, &metadata3, 1000 );
     nb_samples = LMS_RecvStream( &rx_streamyb, buffyb, buffer_size, &metadata4, 1000 );
-
     maxinbuf1=maxinbuf2=maxinbuf3=maxinbuf4=0;
     midinbuf1=midinbuf2=midinbuf3=midinbuf4=0;
     for (int pntr=0; pntr<buffer_size; pntr++) {
@@ -171,13 +181,11 @@ double cpu_time_used;
       if (tmpc>maxinbuf3) maxinbuf3=tmpc;
       if (tmpd>maxinbuf4) maxinbuf4=tmpd;
     }
-//    printf(" %lld %lld %lld %lld ",metadata1.timestamp, metadata2.timestamp, metadata3.timestamp,metadata4.timestamp );
-    printf(" %lld %lld %lld %lld ",metadata1.timestamp, metadata3.timestamp, (metadata1.timestamp-prevtsx),  (metadata3.timestamp-prevtsy));
-   
+ 
+   printf(" %lld %lld %lld %lld ",metadata1.timestamp, metadata3.timestamp, (metadata1.timestamp-prevtsx),  (metadata3.timestamp-prevtsy));
     printf("%5.0f %3.0f   %5.0f %3.0f   %5.0f %3.0f   %5.0f %3.0f  --",sqrt(maxinbuf1),sqrt(midinbuf1),sqrt(maxinbuf2),sqrt(midinbuf2),sqrt(maxinbuf3),sqrt(midinbuf3),sqrt(maxinbuf4),sqrt(midinbuf4));
     prevtsx=metadata1.timestamp;
     prevtsy=metadata3.timestamp;
-
     end = clock(); //debug time
     cpu_time_used = 1000*((double) (end - start))/ CLOCKS_PER_SEC;;
     printf("iteration time=%.3fmilisec  speed=%.1fMbit/sec\n",cpu_time_used,64*buffer_size/1000/cpu_time_used);
@@ -185,7 +193,6 @@ double cpu_time_used;
   send = clock(); //debug time
   cpu_time_used = 1000*((double)(send - sstart)) / CLOCKS_PER_SEC;
   printf("all iteration time = %.3fmilisec middle=%.1fMbps\n",cpu_time_used,mn*64*buffer_size/1000/cpu_time_used);
-
   LMS_StopStream(&rx_streamxa);
   LMS_StopStream(&rx_streamxb);
   LMS_StopStream(&rx_streamya);
